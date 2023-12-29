294d2f1d24b02a293a11a68f167767ec58ecea26
romeusorionaet <romeusoares14569@gmail.com>
2023-12-29 13:49:32 -0300

    first commit

---
+++ src/utils/extract-query-params.js
@@ -0,0 +1,9 @@
+export function extractQueryParams(query) {
+    return query.substr(1).split('&').reduce((queryParams, params) => {
+        const [key, value] = params.split('=')
+
+        queryParams[key] = value
+
+        return queryParams
+    }, {})
+}
---
+++ streams/buffer.js
@@ -0,0 +1,3 @@
+const buf = Buffer.from("Romeu")
+
+console.log(buf)
